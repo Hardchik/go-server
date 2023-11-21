d28e3e058b3dafdb3a54f5a567cb191f21bad72d
unknown <chhabrahardik345@gmail.com>
2023-11-21 23:48:06 +0530

    bugs

--- go.mod
+++ go.mod
@@ -3,25 +3,26 @@ module go-server
 go 1.21.4
 
 require (
-	github.com/gofiber/fiber/v2 v2.51.0
-	github.com/joho/godotenv v1.5.1
-	gorm.io/driver/postgres v1.5.4
-	gorm.io/gorm v1.25.5
+	github.com/gofiber/fiber/v2 v2.30.0
+	github.com/joho/godotenv v1.4.0
+	gorm.io/driver/postgres v1.3.3
+	gorm.io/gorm v1.23.3
 )
 
 require (
 	github.com/andybalholm/brotli v1.0.5 // indirect
-	github.com/google/uuid v1.4.0 // indirect
+	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
+	github.com/jackc/pgconn v1.11.0 // indirect
+	github.com/jackc/pgio v1.0.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgproto3/v2 v2.2.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
-	github.com/jackc/pgx/v5 v5.4.3 // indirect
+	github.com/jackc/pgtype v1.10.0 // indirect
+	github.com/jackc/pgx/v4 v4.15.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/klauspost/compress v1.16.7 // indirect
-	github.com/mattn/go-colorable v0.1.13 // indirect
-	github.com/mattn/go-isatty v0.0.20 // indirect
-	github.com/mattn/go-runewidth v0.0.15 // indirect
-	github.com/rivo/uniseg v0.2.0 // indirect
+	github.com/stretchr/testify v1.8.1 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.50.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"go-server/models"
 	"go-server/storage"
 	"log"
@@ -13,57 +14,42 @@ import (
 )
 
 type Book struct {
-	Author       string    `json:"author"`
-	Title        string    `json:"title"`
-	Publisher    string    `json:"publisher"`
+	Author    string `json:"author"`
+	Title     string `json:"title"`
+	Publisher string `json:"publisher"`
 }
 
 type Repository struct {
 	DB *gorm.DB
 }
 
-func(r *Repository) CreateBook(context *fiber.Ctx)  error{
+func (r *Repository) CreateBook(context *fiber.Ctx) error {
 	book := Book{}
 
 	err := context.BodyParser(&book)
+
 	if err != nil {
 		context.Status(http.StatusUnprocessableEntity).JSON(
 			&fiber.Map{"message": "request failed"})
-			return err
+		return err
 	}
 
-	er := r.DB.Create(&book).Error
+	err = r.DB.Create(&book).Error
 	if err != nil {
-		context.Status(http.StatusBadRequest).JSON(&fiber.Map{
-			"message": "could not create book"})
-		 return er
-	}
-
-	context.Status(http.StatusOK).JSON(&fiber.Map{"message":"book has been added"})
-	return nil
-}
-
-func(r *Repository) GetBooks(context *fiber.Ctx) error{
-	bookModels := &[]models.Books{}
-
-	err := r.DB.Find(bookModels).Error
-	if err != nil {
-		context.Status(http.StatusBadRequest).JSON(&fiber.Map{
-			"message": "could not get the books"})
-		 return err
+		context.Status(http.StatusBadRequest).JSON(
+			&fiber.Map{"message": "could not create book"})
+		return err
 	}
 
 	context.Status(http.StatusOK).JSON(&fiber.Map{
-		"message":"books fetched successfully",
-		"data": bookModels,
-	})
+		"message": "book has been added"})
 	return nil
 }
 
-func(r *Repository) DeleteBook(context *fiber.Ctx) error{
+func (r *Repository) DeleteBook(context *fiber.Ctx) error {
 	bookModel := models.Books{}
 	id := context.Params("id")
-	if id == ""{
+	if id == "" {
 		context.Status(http.StatusInternalServerError).JSON(&fiber.Map{
 			"message": "id cannot be empty",
 		})
@@ -78,43 +64,59 @@ func(r *Repository) DeleteBook(context *fiber.Ctx) error{
 		})
 		return err.Error
 	}
-
 	context.Status(http.StatusOK).JSON(&fiber.Map{
-		"message": "books delete successfully",
+		"message": "book delete successfully",
 	})
 	return nil
 }
 
-func(r *Repository) GetBookByID(context *fiber.Ctx) error{
-	bookModel := &models.Books{}
+func (r *Repository) GetBooks(context *fiber.Ctx) error {
+	bookModels := &[]models.Books{}
+
+	err := r.DB.Find(bookModels).Error
+	if err != nil {
+		context.Status(http.StatusBadRequest).JSON(
+			&fiber.Map{"message": "could not get books"})
+		return err
+	}
+
+	context.Status(http.StatusOK).JSON(&fiber.Map{
+		"message": "books fetched successfully",
+		"data":    bookModels,
+	})
+	return nil
+}
+
+func (r *Repository) GetBookByID(context *fiber.Ctx) error {
+
 	id := context.Params("id")
-	if id == ""{
+	bookModel := &models.Books{}
+	if id == "" {
 		context.Status(http.StatusInternalServerError).JSON(&fiber.Map{
 			"message": "id cannot be empty",
 		})
 		return nil
 	}
 
-	err := r.DB.Where("id = ?", id).First(bookModel).Error
+	fmt.Println("the ID is", id)
 
+	err := r.DB.Where("id = ?", id).First(bookModel).Error
 	if err != nil {
-		context.Status(http.StatusBadRequest).JSON(&fiber.Map{
-			"message": "could not get the book",
-		})
+		context.Status(http.StatusBadRequest).JSON(
+			&fiber.Map{"message": "could not get the book"})
 		return err
 	}
-
 	context.Status(http.StatusOK).JSON(&fiber.Map{
 		"message": "book id fetched successfully",
-		"data": bookModel,
+		"data":    bookModel,
 	})
 	return nil
 }
 
-func(r *Repository) SetupRoutes(app *fiber.App) {
+func (r *Repository) SetupRoutes(app *fiber.App) {
 	api := app.Group("/api")
 	api.Post("/create_books", r.CreateBook)
-	api.Delete("/delete_book/:id", r.DeleteBook)
+	api.Delete("delete_book/:id", r.DeleteBook)
 	api.Get("/get_books/:id", r.GetBookByID)
 	api.Get("/books", r.GetBooks)
 }
@@ -125,12 +127,12 @@ func main() {
 		log.Fatal(err)
 	}
 	config := &storage.Config{
-		Host: os.Getenv("DB_HOST"),
-		Port: os.Getenv("DB_PORT"),
+		Host:     os.Getenv("DB_HOST"),
+		Port:     os.Getenv("DB_PORT"),
 		Password: os.Getenv("DB_PASS"),
-		User: os.Getenv("DB_USER"),
-		SSLMode: os.Getenv("DB_SSL"),
-		DBName: os.Getenv("DB_NAME"),
+		User:     os.Getenv("DB_USER"),
+		SSLMode:  os.Getenv("DB_SSLMODE"),
+		DBName:   os.Getenv("DB_NAME"),
 	}
 
 	db, err := storage.NewConnection(config)
@@ -138,9 +140,8 @@ func main() {
 	if err != nil {
 		log.Fatal("could not load the database")
 	}
-
-	er := models.MigrateBooks(db)
-	if er != nil {
+	err = models.MigrateBooks(db)
+	if err != nil {
 		log.Fatal("could not migrate db")
 	}
 
